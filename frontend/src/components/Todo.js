fe91a94661c64e741951a0ae8b26e0334260de2c
nideshpandey <nideshpy@gmail.com>
2022-07-09 12:14:26 +0545

    Delete Logic added

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
 
-function Todo({todo}) {
+function Todo({ todo }) {
+  const handleClick = async () => {
+    const response = await fetch("/api/todos/" + todo._id, {
+        method: 'DELETE'
+    });
+
+    const json = await response.json();
+
+    if (response.ok) {
+      console.log("Todo Deleted");
+      console.log(json);
+    }
+  };
   return (
     <div className="todo-details">
-        <h4>{todo.title}</h4>
-        <h3>{todo.description}</h3>
-        <p>{todo.createdAt}</p>
-        <br />
-
+      <h4>{todo.title}</h4>
+      <h3>{todo.description}</h3>
+      <p>{todo.createdAt}</p>
+      <button onClick={handleClick}>Delete</button>
+      <br />
     </div>
-  )
+  );
 }
 
-export default Todo
+export default Todo;
