1c0112d807556c2faf4b0c5214b55e76b66aca67
nideshpandey <nideshpy@gmail.com>
2022-07-09 18:41:53 +0545

    Added date format package

--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { formatDistanceToNow } from "date-fns";
 
 function Todo({ todo }) {
   const handleClick = async () => {
@@ -17,8 +18,12 @@ function Todo({ todo }) {
     <div className="todo-details">
       <h3>{todo.title}</h3>
       <h5>{todo.description}</h5>
-      <p>{todo.createdAt}</p>
-      <button class="material-symbols-outlined" onClick={handleClick}>delete</button>
+      <p>
+        {formatDistanceToNow(new Date(todo.createdAt), { addSuffix: true })}
+      </p>
+      <button class="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </button>
       <br />
     </div>
   );
